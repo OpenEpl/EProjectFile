5ca8d3c00b147a5b70490f2afe8152b6d939f075
ArcticLampyrid <ArcticLampyrid@outlook.com>
2022-07-07 23:14:26 +0800

    feature: support to read ECDependencies Section

---
+++ EProjectFile/ECDependenciesSectionInfo.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace QIQI.EProjectFile
+{
+    public class ECDependenciesSectionInfo
+    {
+        public const string SectionName = "易模块记录段";
+        public const int SectionKey = 0x0C007319;
+        public List<ECDependencyInfo> ECDependencies { get; set; }
+        public static ECDependenciesSectionInfo Parse(byte[] data, Encoding encoding)
+        {
+            var that = new ECDependenciesSectionInfo();
+            using (var reader = new BinaryReader(new MemoryStream(data, false), encoding))
+            {
+                var count = reader.ReadInt32();
+                that.ECDependencies = new List<ECDependencyInfo>(count);
+                for (int i = 0; i < count; i++)
+                {
+                    var dependency = new ECDependencyInfo();
+                    dependency.InfoVersion = reader.ReadInt32();
+                    if (dependency.InfoVersion > 2)
+                    {
+                        throw new Exception($"ECDependencyInfo.InfoVersion = {dependency.InfoVersion}, not supported");
+                    }
+                    dependency.UnknownInt1 = reader.ReadInt32();
+                    dependency.UnknownInt2 = reader.ReadInt32();
+                    dependency.UnknownInt3 = reader.ReadInt32();
+                    if (dependency.InfoVersion >= 2)
+                    {
+                        dependency.UnknownInt4 = reader.ReadInt32();
+                    }
+                    dependency.Name = reader.ReadStringWithLengthPrefix(encoding);
+                    dependency.Path = reader.ReadStringWithLengthPrefix(encoding);
+                    var starts = reader.ReadInt32sWithByteSizePrefix();
+                    var counts = reader.ReadInt32sWithByteSizePrefix();
+                    dependency.DefinedIds = new List<ECDependencyInfo.PackedIds>(starts.Length);
+                    for (int indexOfids = 0; indexOfids < starts.Length; indexOfids++)
+                    {
+                        dependency.DefinedIds.Add(new ECDependencyInfo.PackedIds
+                        {
+                            Start = starts[indexOfids],
+                            Count = counts[indexOfids]
+                        });
+                    }
+                    that.ECDependencies.Add(dependency);
+                }
+            }
+            return that;
+        }
+        public byte[] ToBytes(Encoding encoding)
+        {
+            byte[] data;
+            using (var writer = new BinaryWriter(new MemoryStream(), encoding))
+            {
+                WriteTo(writer, encoding);
+                writer.Flush();
+                data = ((MemoryStream)writer.BaseStream).ToArray();
+            }
+            return data;
+        }
+        private void WriteTo(BinaryWriter writer, Encoding encoding)
+        {
+            writer.Write(ECDependencies.Count);
+            foreach (var dependency in ECDependencies)
+            {
+                writer.Write(dependency.InfoVersion);
+                writer.Write(dependency.UnknownInt1);
+                writer.Write(dependency.UnknownInt2);
+                writer.Write(dependency.UnknownInt3);
+                if (dependency.InfoVersion >= 2)
+                {
+                    writer.Write(dependency.UnknownInt4);
+                }
+                writer.WriteInt32sWithByteSizePrefix(dependency.DefinedIds.Select(x => x.Start).ToArray());
+                writer.WriteInt32sWithByteSizePrefix(dependency.DefinedIds.Select(x => x.Count).ToArray());
+            }
+        }
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+}
---
+++ EProjectFile/ECDependencyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QIQI.EProjectFile
+{
+    public class ECDependencyInfo
+    {
+        public struct PackedIds
+        {
+            public int Start;
+            public int Count;
+        }
+
+        public int InfoVersion { get; set; }
+        public int UnknownInt1 { get; set; }
+        public int UnknownInt2 { get; set; }
+        public int UnknownInt3 { get; set; }
+        public int UnknownInt4 { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public List<PackedIds> DefinedIds { get; set; }
+    }
+}
