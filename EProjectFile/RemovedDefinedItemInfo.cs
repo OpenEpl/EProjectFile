baf4a54ad3b77e3b633d991f489a27a438450d83
ArcticLampyrid <ArcticLampyrid@outlook.com>
2022-07-25 16:43:45 +0800

    feature: calculate name code for `RemovedDefinedItemInfo`

--- EProjectFile/RemovedDefinedItemInfo.cs
+++ EProjectFile/RemovedDefinedItemInfo.cs
@@ -11,8 +11,6 @@ namespace QIQI.EProjectFile
     {
         public int Id { get; }
         public string Name { get; set; }
-        [JsonIgnore]
-        public int UnknownAfterId { get; set; }
         public RemovedDefinedItemInfo(int id)
         {
             this.Id = id;
@@ -25,17 +23,23 @@ namespace QIQI.EProjectFile
             var endPosition = reader.BaseStream.Position + size;
             var result = new RemovedDefinedItemInfo[count];
             var ids = reader.ReadInt32sWithFixedLength(count);
-            var unknownAfterIds = reader.ReadInt32sWithFixedLength(count);
+            var nameCode = reader.ReadInt32sWithFixedLength(count);
             var offsets = reader.ReadInt32sWithFixedLength(count);
             var startPosition = reader.BaseStream.Position;
             for (int i = 0; i < count; i++)
             {
                 reader.BaseStream.Position = startPosition + offsets[i];
+                var name = reader.ReadCStyleString(encoding);
                 result[i] = new RemovedDefinedItemInfo(ids[i])
                 {
-                    Name = reader.ReadCStyleString(encoding),
-                    UnknownAfterId = unknownAfterIds[i]
+                    Name = name,
                 };
+                var calculatedNameCode = CalculateNameCode(name, encoding);
+                if (nameCode[i] != calculatedNameCode)
+                {
+                    throw new Exception($"Invaild name code for \"{name}\" ({encoding.WebName}), "
+                        + $"calculated 0x{calculatedNameCode:X8} but got 0x{nameCode[i]:X8}");
+                }
             }
             reader.BaseStream.Position = endPosition;
             return result;
@@ -72,9 +76,37 @@ namespace QIQI.EProjectFile
             writer.Write(count);
             writer.Write((count * 12) + elem.Sum(x => x.Length));
             Array.ForEach(data, x => writer.Write(x.Id));
-            Array.ForEach(data, x => writer.Write(x.UnknownAfterId));
+            Array.ForEach(data, x => writer.Write(CalculateNameCode(x.Name, encoding)));
             writer.WriteInt32sWithoutLengthPrefix(offsets);
             Array.ForEach(elem, x => writer.Write(x));
+        }
+
+        private static int CalculateNameCode(string name, Encoding encoding)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return 0x01000000;
+            }
+            var bytes = encoding.GetBytes(name);
+            byte composition1 = unchecked((byte)(bytes.Length + 1));
+            ushort composition2 = 0;
+            foreach (var item in bytes)
+            {
+                unchecked
+                {
+                    // 为了保持与易语言内部算法一致，不应当考虑双字节问题
+                    // 否则形如以下编码的字符将无法被计算出正确结果
+                    // 護 D76F (GB2312)
+                    // 譸 D770 (GB2312)
+                    if (item >= 'a' && item <= 'z')
+                    {
+                        composition2 -= 0x20;
+                    }
+                    composition2 += item;
+                }
+            }
+            byte composition3 = bytes[0];
+            return (composition1 << 24) | (composition2 << 8) | composition3;
         }
     }
 
