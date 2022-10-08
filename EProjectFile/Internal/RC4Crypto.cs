21824f61f4d26e181dbb93f48527b02090576853
ArcticLampyrid <ArcticLampyrid@outlook.com>
2022-10-08 10:46:05 +0800

    optimize: RC4Crypto

--- EProjectFile/Internal/RC4Crypto.cs
+++ EProjectFile/Internal/RC4Crypto.cs
@@ -7,26 +7,37 @@ namespace QIQI.EProjectFile.Internal
 {
     internal class RC4Crypto : ICloneable
     {
-        private byte[] status;
+        private readonly byte[] status;
         private int i = 0;
         private int j = 0;
-        public RC4Crypto(byte[] initialStatus)
-        {
-            status = initialStatus ?? throw new ArgumentNullException(nameof(initialStatus));
-            status = (byte[])status.Clone();
-        }
+        /// <param name="key">It can be null</param>
+        /// <param name="initialStatus">It cannot be null</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public RC4Crypto(byte[] key, byte[] initialStatus)
         {
-            status = initialStatus ?? throw new ArgumentNullException(nameof(initialStatus));
-            status = (byte[])status.Clone();
-
+            status = (byte[])initialStatus?.Clone() ?? throw new ArgumentNullException(nameof(initialStatus));
+            EmitKey(key);
+        }
+        public RC4Crypto(byte[] key, int statusLength)
+        {
+            status = new byte[statusLength];
+            for (i = 0; i < statusLength; i++)
+            {
+                status[i] = unchecked((byte)i);
+            }
+            EmitKey(key);
+        }
+        private void EmitKey(byte[] key)
+        {
+            if (key is null)
+            {
+                return;
+            }
             int p2 = 0;
             for (int p1 = 0; p1 < status.Length; p1++)
             {
                 p2 = (p2 + status[p1] + key[p1 % key.Length]) % status.Length;
-                byte temp = status[p1];
-                status[p1] = status[p2];
-                status[p2] = temp;
+                (status[p2], status[p1]) = (status[p1], status[p2]);
             }
         }
         public void Decode(byte[] data, long start, long length)
@@ -36,9 +47,7 @@ namespace QIQI.EProjectFile.Internal
             {
                 i = (i + 1) % status.Length;
                 j = (j + status[i]) % status.Length;
-                byte temp = status[i];
-                status[i] = status[j];
-                status[j] = temp;
+                (status[j], status[i]) = (status[i], status[j]);
                 if (data != null)
                 {
                     data[offset] ^= status[(status[i] + status[j]) % status.Length];
@@ -49,6 +58,6 @@ namespace QIQI.EProjectFile.Internal
         public void Decode(byte[] data) => Decode(data, 0, data.Length);
         public void Encode(byte[] data) => Encode(data, 0, data.Length);
         public void Skip(long length) => Decode(null, 0, length);
-        public object Clone() => new RC4Crypto(status) { i = i, j = j };
+        public object Clone() => new RC4Crypto(null, status) { i = i, j = j };
     }
 }
